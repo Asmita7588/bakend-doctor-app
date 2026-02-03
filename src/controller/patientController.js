0082c2e5e08139cd3b87c22a5b05b10f0ed99100
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2026-02-03 19:42:52 +0530

    [Asmita]Add:implemented radis cache , and modified swagger settings

--- src/controller/patientController.js
+++ src/controller/patientController.js
@@ -1,4 +1,5 @@
 import * as patientService from "../services/patientService.js";
+import radis from "../config/radisClient.js";
 
 export const addPatient = async (req, res) => {
     try {
@@ -10,9 +11,23 @@ export const addPatient = async (req, res) => {
 };
 
 export const getPatients = async (req, res) => {
+       const cacheKey = "patients_data";
     try {
+        const cachedData = await radis.get(cacheKey);
+        if (cachedData) {
+            console.log("Getting data from Redis cache");
+            return res.json({
+                success: true,
+                message: "fetched from cache",
+                data: JSON.parse(cachedData)
+            });
+        }
+
         const patients = await patientService.getAllPatients();
-        res.status(200).json({ success: true, data: patients });
+
+        //store in redis for 10 min
+        await radis.setEx(cacheKey, 600, JSON.stringify(patients));
+        res.status(200).json({message:"fetching data from db", count: patients.length, success: true, data: patients });
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
     }
--- swagger.js
+++ swagger.js
@@ -2,16 +2,27 @@ import swaggerAutogen from 'swagger-autogen';
 
 const doc = {
   info: {
-    title: 'My API',
-    description: 'Description'
+    title: 'Doctor App API',
+    description: 'Hospital Management System'
   },
-  host: 'localhost:3000'
+  host: 'localhost:3000',
+  securityDefinitions: {
+    bearerAuth: {
+      type: 'apiKey',
+      name: 'Authorization',
+      in: 'header',
+      description: 'Enter your bearer token in the format: Bearer <token>'
+    }
+  },
+  security: [
+    {
+      bearerAuth: []
+    }
+  ]
 };
 
 const outputFile = './swagger.json';
 const routes = ['./index.js'];
 
-/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
-root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
 swaggerAutogen()(outputFile, routes, doc)
